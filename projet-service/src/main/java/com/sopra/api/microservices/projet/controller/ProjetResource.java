b1324ef89b1714bc3e1e3e6a638c2f86260e5dcf
mohabouha <bouhamydhassangmail.com>
2017-06-10 09:58:42 +0200

    Revert "msj msg projet service"
    This reverts commit 65346148e3d8b874ae10982b0dd891a64b428b3d.

--- projet-service/src/main/java/com/sopra/api/microservices/projet/controller/ProjetResource.java
+++ projet-service/src/main/java/com/sopra/api/microservices/projet/controller/ProjetResource.java
@@ -16,7 +16,7 @@ public class ProjetResource {
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String findClients() {
-        return "Hello World !!";
+        return "Hello World";
     }
 
 }
